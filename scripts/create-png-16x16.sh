0ccac634093243398de0ed05a3481997305e6513
Caro <carola.hp@gmail.com>
2022-11-02 11:47:32 -0300

    Scripts to create icons with custom size (first version)

---
+++ scripts/create-png-16x16.sh
@@ -0,0 +1,3 @@
+/Applications/Inkscape.app/Contents/MacOS/inkscape ../svg/glamorousBrowse.svg --export-type="png" -w 16 -h 16
+/Applications/Inkscape.app/Contents/MacOS/inkscape ../svg/glamorousOpenFromUrl.svg --export-type="png" -w 16 -h 16
+/Applications/Inkscape.app/Contents/MacOS/inkscape ../svg/smallInfo.svg --export-type="png" -w 16 -h 16
